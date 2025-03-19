e1b61a20e870680525c57d4beb79ec8e51d67788
ItaloGustavoS <42496107+ItaloGustavoS@users.noreply.github.com>
2025-03-19 11:15:41 -0300

    Refactor router component to use BrowserRouter and update route paths

--- meu-projeto-react/src/router.jsx
+++ meu-projeto-react/src/router.jsx
@@ -1,16 +1,17 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from "./App"; // Componente da página inicial que você já tem
-import ChatPage from "./ChatPage"; // Componente de chat que acabamos de criar
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App"; // Ajuste o caminho de acordo com sua estrutura de pastas
+import ChatPage from "./ChatPage"; // Crie esta página ou ajuste o caminho conforme necessário
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<App />} />
         <Route path="/chat" element={<ChatPage />} />
+        {/* Adicione outras rotas aqui conforme necessário */}
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
