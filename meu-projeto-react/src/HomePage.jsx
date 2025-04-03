48ea77075940fc33a1f5e0a88e20baa139a0823b
ItaloGustavoS <42496107+ItaloGustavoS@users.noreply.github.com>
2025-04-03 16:02:06 -0300

    Increase particle count to 200 for enhanced visual effects on HomePage

--- meu-projeto-react/src/HomePage.jsx
+++ meu-projeto-react/src/HomePage.jsx
@@ -13,7 +13,7 @@ function HomePage() {
       window.particlesJS("particles-js", {
         particles: {
           number: {
-            value: 100,
+            value: 200,
             density: {
               enable: true,
               value_area: 800,
